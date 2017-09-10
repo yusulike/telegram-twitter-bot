8e86f616513c32fed9e6ffb4e8ede78f256ff877
yusu.kang@gmail.com <yusu.kang@gmail.com>
2017-09-10 17:12:54 +0900

    changed tab to space.wq

--- bot.py
+++ bot.py
@@ -5,38 +5,38 @@ from telegram import Updater
 
 #command handlers
 def subscribe(bot, update):
-	if update.message.chat_id not in common.subscribers:
-		common.subscribers.append(update.message.chat_id)
-		bot.sendMessage(update.message.chat_id, text='Subscribed!')
-		common.saveSubscribers(common.subscribers)
-	else:
-		bot.sendMessage(update.message.chat_id, text='Already Subscribed!')
+    if update.message.chat_id not in common.subscribers:
+        common.subscribers.append(update.message.chat_id)
+        bot.sendMessage(update.message.chat_id, text='Subscribed!')
+        common.saveSubscribers(common.subscribers)
+    else:
+        bot.sendMessage(update.message.chat_id, text='Already Subscribed!')
 
 
 def unsubscribe(bot, update):
-	if update.message.chat_id in common.subscribers:
-		common.subscribers.remove(update.message.chat_id)
-		bot.sendMessage(update.message.chat_id, text='Unsubscribed!')
-		common.saveSubscribers(common.subscribers)
-	else:
-		bot.sendMessage(update.message.chat_id, text='You need to subscribe first!')
+    if update.message.chat_id in common.subscribers:
+        common.subscribers.remove(update.message.chat_id)
+        bot.sendMessage(update.message.chat_id, text='Unsubscribed!')
+        common.saveSubscribers(common.subscribers)
+    else:
+        bot.sendMessage(update.message.chat_id, text='You need to subscribe first!')
 
 def bot_main(bot_token=""):
-	# Create the EventHandler and pass it your bot's token.
-	updater = Updater(bot_token)
+    # Create the EventHandler and pass it your bot's token.
+    updater = Updater(bot_token)
 
-	common.bot = updater.bot
+    common.bot = updater.bot
 
-	# Get the dispatcher to register handlers
-	dp = updater.dispatcher
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
 
-	# on different commands - answer in Telegram
-	dp.addTelegramCommandHandler("subscribe", subscribe)
-	dp.addTelegramCommandHandler("unsubscribe", unsubscribe)
+    # on different commands - answer in Telegram
+    dp.addTelegramCommandHandler("subscribe", subscribe)
+    dp.addTelegramCommandHandler("unsubscribe", unsubscribe)
 
-	# Start the Bot
-	updater.start_polling(timeout=5)
+    # Start the Bot
+    updater.start_polling(timeout=5)
 
-	# Run the bot until the user presses Ctrl-C or the process receives SIGINT,
-	# SIGTERM or SIGABRT
-	updater.idle()
+    # Run the bot until the user presses Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT
+    updater.idle()
--- stream.py
+++ stream.py
@@ -6,23 +6,23 @@ import tweepy
 
 class TweetsStreamListener(tweepy.StreamListener):
 
-	def on_connect(self):
-		print("Connected to stream")
+    def on_connect(self):
+        print("Connected to stream")
 
-	def on_status(self, status):
-		if not hasattr(status, 'retweeted_status'):
-			for chat_id in common.subscribers:
-				common.bot.sendMessage(chat_id, "@{screen_name}: {text}".format(screen_name=status.user.screen_name, text=status.text))
+    def on_status(self, status):
+        if not hasattr(status, 'retweeted_status'):
+            for chat_id in common.subscribers:
+                common.bot.sendMessage(chat_id, "@{screen_name}: {text}".format(screen_name=status.user.screen_name, text=status.text))
 
-			try:
-				print("{screen_name}: {text}".format(screen_name=status.user.screen_name, text=status.text))
-			except UnicodeEncodeError:
-				pass
+            try:
+                print("{screen_name}: {text}".format(screen_name=status.user.screen_name, text=status.text))
+            except UnicodeEncodeError:
+                pass
 
 
-	def on_error(self, status_code):
-		if status_code == 420:
-			print("Reached connection threshold to twitter server. disconnecting")
-			return False
+    def on_error(self, status_code):
+        if status_code == 420:
+            print("Reached connection threshold to twitter server. disconnecting")
+            return False
 
-		print("error status: {status_code}".format(status_code=status_code))
+        print("error status: {status_code}".format(status_code=status_code))
